29615c5739d8af1cc29da63b560f005572974441
thousand0001 <thousand0001@thousand0001deMacBook-Air.local>
2021-07-18 13:58:56 +0800

    july_18_2021

--- hello_world/hello_world.py
+++ hello_world/hello_world.py
@@ -2,4 +2,4 @@ print('hello world')
 for i in range(1,10):
     for j in range(1,10):
         print("%d*%d=%2d"%(i,j,i*j),end=' ')
-    print(' ')
+    print('', end='\n')
---
+++ second_world/my_second.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_excel("/Users/thousand0001/Documents/python/second_world/1.xlsx")
+print(df)
+df.to_excel("/Users/thousand0001/Documents/python/second_world/2.xlsx")
